65aa54c5b4281c48fe773a10f5a00b49725f6afe
Austin Moore <austin1moore@gmail.com>
2022-11-08 17:44:32 -0600

    added function for weatherdata

--- assets/script.js
+++ assets/script.js
@@ -29,38 +29,142 @@ function handleFormSubmit(event) {
   liButton.onclick = SearchHistorySearch;
 }
 
-function SearchHistorySearch(){
-    if (storage.length > 0) {
-        getWeatherdata(storage[storage.length - 1]);
-    }
-    }
+function SearchHistorySearch() {
+  if (storage.length > 0) {
+    getWeatherdata(storage[storage.length - 1]);
+  }
+}
 
-    button.addEventListener("click", handleFormSubmit)
-    function getWeatherdata(userInput) {
-    //this api gets Lat and long data
-        fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${userInput}&appid=${apiKey}`)
-            .then(function (response) {
-                return response.json()
-    
-            })
-            .then(function (cityData) {
-    
-                console.log(cityData[0].lat);
-                console.log(cityData[0].lon);
-                return { lat: cityData[0].lat, lon: cityData[0].lon }
-    
-            })
-            
-            .then(function (latLonData) {
-                return fetch(`https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=${latLonData.lat}&lon=${latLonData.lon}&appid=${apiKey}`)
-                    .then(function (response) {
-                        return response.json()
-                    })
-                    .then(function (data) {
-                        const weatherInfo = data
-                        const weatherArray = []
-                        for (let i = 0; i < weatherInfo.list.length; i += 7) {
-    
-                            weatherArray.push(weatherInfo.list[i])
-    
-                        }
+button.addEventListener("click", handleFormSubmit);
+function getWeatherdata(userInput) {
+  //this api gets Lat and long data
+  fetch(
+    `https://api.openweathermap.org/geo/1.0/direct?q=${userInput}&appid=${apiKey}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (cityData) {
+      console.log(cityData[0].lat);
+      console.log(cityData[0].lon);
+      return { lat: cityData[0].lat, lon: cityData[0].lon };
+    })
+
+    .then(function (latLonData) {
+      return fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=${latLonData.lat}&lon=${latLonData.lon}&appid=${apiKey}`
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          const weatherInfo = data;
+          const weatherArray = [];
+          for (let i = 0; i < weatherInfo.list.length; i += 7) {
+            weatherArray.push(weatherInfo.list[i]);
+          }
+          console.log(weatherArray);
+          console.log("weatherInfo", weatherInfo);
+          console.log(weatherArray);
+          let temp = document.getElementById("temp");
+          let feelsLike = document.getElementById("feelsLike");
+          let humidity = document.getElementById("humidity");
+          let windSpeed = document.getElementById("windSpeed");
+          temp.textContent = "temp: " + weatherArray[0].main.temp + "F";
+          feelsLike.textContent =
+            "feels Like: " + weatherArray[0].main.feels_like + "F";
+          humidity.textContent =
+            "Humidity " + weatherArray[0].main.humidity + "%";
+          windSpeed.textContent =
+            "Wind Speed " + weatherArray[0].wind.speed + "mph";
+          let cityDay = document.getElementById("cityDay");
+          cityDay.textContent =
+            weatherInfo.city.name + " (" + weatherArray[0].dt_txt + ")";
+          let iconEl = document.getElementById("iconEl");
+          iconEl.src =
+            "https://openweathermap.org/img/w/" +
+            weatherInfo.list[0].weather[0].icon +
+            ".png";
+
+          let icon1 = document.getElementById("icon1");
+          icon1.src =
+            "https://openweathermap.org/img/w/" +
+            weatherInfo.list[7].weather[0].icon +
+            ".png";
+          let date1 = document.getElementById("date1");
+          date1.textContent = weatherArray[1].dt_txt;
+          let temp1 = document.getElementById("temp1");
+          temp1.textContent = "temp: " + weatherArray[1].main.temp + "F";
+          let wind1 = document.getElementById("wind1");
+          wind1.textContent =
+            "wind speed:" + weatherArray[1].wind.speed + "mph";
+          let humid1 = document.getElementById("humid1");
+          humid1.textContent = "Humidity" + weatherArray[1].main.humidity + "%";
+
+          let icon2 = document.getElementById("icon2");
+          icon2.src =
+            "https://openweathermap.org/img/w/" +
+            weatherInfo.list[15].weather[0].icon +
+            ".png";
+          let date2 = document.getElementById("date2");
+          date2.textContent = weatherArray[2].dt_txt;
+          let temp2 = document.getElementById("temp2");
+          temp2.textContent = "temp: " + weatherArray[2].main.temp + "F";
+          let wind2 = document.getElementById("wind2");
+          wind2.textContent =
+            "wind speed:" + weatherArray[2].wind.speed + "mph";
+          let humid2 = document.getElementById("humid2");
+          humid2.textContent = "Humidity" + weatherArray[2].main.humidity + "%";
+
+          let icon3 = document.getElementById("icon3");
+          icon3.src =
+            "https://openweathermap.org/img/w/" +
+            weatherInfo.list[23].weather[0].icon +
+            ".png";
+          let date3 = document.getElementById("date3");
+          date3.textContent = weatherArray[3].dt_txt;
+          let temp3 = document.getElementById("temp3");
+          temp3.textContent = "temp: " + weatherArray[3].main.temp + "F";
+          let wind3 = document.getElementById("wind3");
+          wind3.textContent =
+            "wind speed:" + weatherArray[3].wind.speed + "mph";
+          let humid3 = document.getElementById("humid3");
+          humid3.textContent = "Humidity" + weatherArray[3].main.humidity + "%";
+
+          let icon4 = document.getElementById("icon4");
+          icon4.src =
+            "https://openweathermap.org/img/w/" +
+            weatherInfo.list[30].weather[0].icon +
+            ".png";
+          let date4 = document.getElementById("date4");
+          date4.textContent = weatherArray[4].dt_txt;
+          let temp4 = document.getElementById("temp4");
+          temp4.textContent = "temp: " + weatherArray[4].main.temp + "F";
+          let wind4 = document.getElementById("wind4");
+          wind4.textContent =
+            "wind speed:" + weatherArray[4].wind.speed + "mph";
+          let humid4 = document.getElementById("humid4");
+          humid4.textContent = "Humidity" + weatherArray[4].main.humidity + "%";
+
+          let icon5 = document.getElementById("icon5");
+          icon5.src =
+            "https://openweathermap.org/img/w/" +
+            weatherInfo.list[39].weather[0].icon +
+            ".png";
+          let date5 = document.getElementById("date5");
+          date5.textContent = weatherArray[5].dt_txt;
+          let temp5 = document.getElementById("temp5");
+          temp5.textContent = "temp: " + weatherArray[5].main.temp + "F";
+          let wind5 = document.getElementById("wind5");
+          wind5.textContent =
+            "wind speed:" + weatherArray[5].wind.speed + "mph";
+          let humid5 = document.getElementById("humid5");
+          humid5.textContent = "Humidity" + weatherArray[5].main.humidity + "%";
+        });
+    });
+}
+
+function check() {
+  userInput = showHistory;
+  getWeatherdata();
+}
