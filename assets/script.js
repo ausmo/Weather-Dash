a3008c5d12202632019f418b1213699b2ea62b71
Austin Moore <austin1moore@gmail.com>
2022-11-08 17:22:11 -0600

    added function for latlong

--- assets/script.js
+++ assets/script.js
@@ -34,3 +34,33 @@ function SearchHistorySearch(){
         getWeatherdata(storage[storage.length - 1]);
     }
     }
+
+    button.addEventListener("click", handleFormSubmit)
+    function getWeatherdata(userInput) {
+    //this api gets Lat and long data
+        fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${userInput}&appid=${apiKey}`)
+            .then(function (response) {
+                return response.json()
+    
+            })
+            .then(function (cityData) {
+    
+                console.log(cityData[0].lat);
+                console.log(cityData[0].lon);
+                return { lat: cityData[0].lat, lon: cityData[0].lon }
+    
+            })
+            
+            .then(function (latLonData) {
+                return fetch(`https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=${latLonData.lat}&lon=${latLonData.lon}&appid=${apiKey}`)
+                    .then(function (response) {
+                        return response.json()
+                    })
+                    .then(function (data) {
+                        const weatherInfo = data
+                        const weatherArray = []
+                        for (let i = 0; i < weatherInfo.list.length; i += 7) {
+    
+                            weatherArray.push(weatherInfo.list[i])
+    
+                        }
